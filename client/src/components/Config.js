ccb0d9d84dd83c54eeab0bec3967bc12065e1324
oaoong <wjdwofkd12@naver.com>
2021-02-12 13:48:57 +0900

    Landing Page

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "e53839e522a3a600e04cb1481fdbaf90";
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
