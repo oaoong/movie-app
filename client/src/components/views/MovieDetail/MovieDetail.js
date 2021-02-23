2b67920d7b2df6aa390c33c94632f587aedbdabf
oaoong <wjdwofkd12@naver.com>
2021-02-23 17:52:46 +0900

    favorite Button

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -3,6 +3,7 @@ import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
 import MainImage from "../LandingPage/Sections/MainImage";
 import MovieInfo from "./Sections/MovieInfo";
 import GridCards from "../commons/GridCards";
+import Favorite from "./Sections/Favorite";
 import { Row } from "antd";
 
 function MovieDetail(props) {
@@ -25,7 +26,6 @@ function MovieDetail(props) {
     fetch(endpointCrew)
       .then((response) => response.json())
       .then((response) => {
-        console.log("responseForCrew", response);
         setCasts(response.cast);
       });
   }, []);
@@ -46,6 +46,14 @@ function MovieDetail(props) {
 
       {/* Body */}
       <div style={{ width: "85%", margin: "1rem auto" }}>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Favorite
+            movieInfo={Movie}
+            movieId={movieId}
+            userFrom={localStorage.getItem("userId")}
+          />
+        </div>
+
         {/* Movie Info */}
 
         <MovieInfo movie={Movie} />
